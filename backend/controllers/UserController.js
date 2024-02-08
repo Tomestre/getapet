fac37286e3ead7a175ce4383dbf3f1373e413198
Tomestre <gabrielveleci@gmail.com>
2024-02-08 11:01:39 -0300

    feat: middleware verify user

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -138,4 +138,29 @@ module.exports = class UserController{
 
         res.status(200).send(currentUser)
     }
+
+    static async getUserById(req, res) {
+
+        const id= req.params.id
+
+        const user = await User.findById(id).select("-password")
+
+        if(!user){
+            res
+            .status(422).json({
+                message:'Usuário não encontrado!',
+            })
+            return
+        }
+
+        res.status(200).json({user})
+    }
+
+    static async editUser(req, res){
+        
+        res.status(200).json({
+                message:'deu certo',
+            })
+            return
+    }
 }
