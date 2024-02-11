9262e7f19dab5b01fe3a313bf1ad3fc14a96424f
Tomestre <gabrielveleci@gmail.com>
2024-02-11 17:48:18 -0300

    feat: create a pet / upload images

--- backend/routes/PetRouter.js
+++ backend/routes/PetRouter.js
@@ -2,6 +2,14 @@ const router = require('express').Router()
 
 const PetController = require ('../controllers/PetController')
 
-router.post('/create', PetController.create)
+//middlwares
+const verifyToken = require('../helpers/verify-token')
+const {imageUpload} = require('../helpers/image-upload')
+
+router.post(
+    '/create',
+     verifyToken,
+     imageUpload.array('images'),
+      PetController.create)
 
 module.exports = router  
